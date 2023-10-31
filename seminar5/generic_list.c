20e8c262e7e4557acc890a2ee7e24b95457ca8cd
Alex-de-bug <113011463+Alex-de-bug@users.noreply.github.com>
2023-10-31 15:02:53 +0300

    fix code after malloc defini

--- seminar5/generic_list.c
+++ seminar5/generic_list.c
@@ -27,10 +27,13 @@ void error(const char *s)
   };                                                                    \
   void list_##type##_push(struct list_##type *list, type value){        \                                                      
     struct list_##type *new_list = malloc(sizeof(struct list_##type));  \
+    if(new_list==NULL){                                                 \
+      abort();                                                          \
+    }                                                                   \
     if(list->next == 0 && list->value == 0){                            \
       list->value = value;                                              \
     }else{                                                              \
-      while(list->next != 0){                                           \                                                             
+      while(list->next != NULL){                                        \                                                             
         list = list->next;                                              \ 
       }                                                                 \                                           
       new_list->value = value;                                          \
@@ -39,7 +42,7 @@ void error(const char *s)
     }                                                                   \
   }                                                                     \
   void list_##type##_print(struct list_##type *list){                   \
-    while(list->next != 0){                                             \
+    while(list->next != NULL){                                          \
       print(list->value);                                               \
       list = list->next;                                                \ 
       newline_print();                                                  \
@@ -52,18 +55,21 @@ DEFINE_LIST(double)
 
 #define list_print(x)                                                   \
   _Generic((x),                                                         \
-      struct list_int64_t*: list_int64_t_print(x),                      \
-      struct list_double*: list_double_print(x),                        \
+      struct list_int64_t*: list_int64_t_print((void*)x),                      \
+      struct list_double*: list_double_print((void*)x),                        \
       default: error("Unsupported operation")) 
 
 #define list_push(list, value)                                          \
   _Generic((list),                                                      \
-      struct list_int64_t*: list_int64_t_push(list, value),             \
-      struct list_double*: list_double_push(list, value),               \
+      struct list_int64_t*: list_int64_t_push((void*)list, value),             \
+      struct list_double*: list_double_push((void*)list, value),               \
       default: error("Unsupported operation")) 
 
 int main(){
   struct list_int64_t* list_int = malloc(sizeof(struct list_int64_t));
+  if(list_int==NULL){
+    abort();
+  }
   list_push(list_int, __OPENCL_MEMORY_SCOPE_ALL_SVM_DEVICES);
   list_push(list_int, __LONG_LONG_MAX__);
   list_push(list_int, __MAC_OS_X_VERSION_MIN_REQUIRED);
@@ -73,6 +79,9 @@ int main(){
   newline_print();
 
   struct list_double *list_double = malloc(sizeof(struct list_int64_t));
+  if(list_double==NULL){
+    abort();
+  }
   list_push(list_double, 54323.3245);
   list_push(list_double, 3245.6412);
   list_push(list_double, 1234.922);
@@ -82,6 +91,9 @@ int main(){
   newline_print();
 
   struct list_int64_t *list_int_2 = malloc(sizeof(struct list_int64_t));
+  if(list_int_2==NULL){
+    abort();
+  }
   list_push(list_int_2, 4);
   list_push(list_int_2, 2);
   list_push(list_int_2, 1);
